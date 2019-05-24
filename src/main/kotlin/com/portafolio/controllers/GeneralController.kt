e28f876eb980b23a9c443a3267347f82a763b2bf
Darwin Salazar <darwin@localhost.localdomain>
2019-05-23 22:25:10 -0500

    Add log in status

--- src/main/kotlin/com/portafolio/controllers/GeneralController.kt
+++ src/main/kotlin/com/portafolio/controllers/GeneralController.kt
@@ -1,5 +1,6 @@
 package com.portafolio.controllers
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -10,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class GeneralController {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     @GetMapping("/status")
-    fun statusController() = ResponseEntity(mapOf("response" to "The microservice is up"), HttpStatus.OK)
+    fun statusController() : ResponseEntity<Map<String, String>>{
+
+        log.info("[statusController] Test for logs in portfolio")
+
+        return ResponseEntity(mapOf("response" to "The microservice 'portafolio_ms' is up"), HttpStatus.OK)
+    }
 
 }
