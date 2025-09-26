824a254196d65eff00deeb29c34b05f390808aa4
DarwinSalaz <dlsg-17@hotmail.com>
2025-09-26 00:13:44 -0500

    fix cargue masivo

--- src/main/kotlin/com/portafolio/controllers/BulkUploadController.kt
+++ src/main/kotlin/com/portafolio/controllers/BulkUploadController.kt
@@ -14,6 +14,7 @@ import java.io.InputStream
 
 @RestController
 @RequestMapping("/api/portfolio/bulk-upload")
+@CrossOrigin(origins = ["*"], methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.OPTIONS], allowedHeaders = ["*"])
 class BulkUploadController {
     
     @Autowired
