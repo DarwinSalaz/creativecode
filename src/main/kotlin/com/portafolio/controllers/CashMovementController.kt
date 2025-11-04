004aba1fc60792923c858eaf17c2c27582db1fd6
DarwinSalaz <darwinsalaz@gmail.com>
2025-11-03 19:52:53 -0500

    Update CashMovementController.kt

--- src/main/kotlin/com/portafolio/controllers/CashMovementController.kt
+++ src/main/kotlin/com/portafolio/controllers/CashMovementController.kt
@@ -1,6 +1,8 @@
 package com.portafolio.controllers
 
 import com.portafolio.dtos.ResumeWalletRequest
+import com.portafolio.dtos.UserMovementsReportRequest
+import com.portafolio.dtos.UserMovementsReportResponse
 import com.portafolio.services.CashMovementService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
@@ -18,5 +20,14 @@ class CashMovementController {
     @PostMapping("/cash_movement/wallet_resume")
     fun getCashMovementsByWallet(@Valid @RequestBody request: ResumeWalletRequest)
         = cashMovementService.getCashMovementsByWallet(request.walletId, request.startsAt, request.endsAt)
+
+    @PostMapping("/cash_movement/user_report")
+    fun getUserMovementsReport(@Valid @RequestBody request: UserMovementsReportRequest): UserMovementsReportResponse {
+        return cashMovementService.getUserMovementsReport(
+            request.applicationUserId, 
+            request.startsAt, 
+            request.endsAt
+        )
+    }
 
 }
